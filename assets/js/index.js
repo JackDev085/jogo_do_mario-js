fd09d6ddc46825e1941f0e008a067a5d16690a16
Jackson Lorran <computadordb@gmail.com>
2024-01-07 13:42:13 -0300

    Adicionando alguns efeitos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,23 @@
 const mario = document.querySelector('.mario'); // Adicione um ponto para indicar uma classe
+const pipe = document.querySelector('.cano'); // Adicione um ponto para indicar uma classe
 
 const jump = () => {
   mario.classList.add('jump'); // Adicione uma classe chamada 'jump'
   setTimeout(()=>{
     mario.classList.remove('jump'); 
 
-  },500);
+  },500)
+};
 
-   
+const loop = setInterval(()=>{
 
-}
+const pipePosition  = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+  
+console.log(marioPosition)
+if (pipePosition <= 120 && pipePosition>0 && marioPosition <100) {
+    pipe.style.animation = 'none';
+    pipe.style.left = '${pipePosition}px'
+  }
+},10);
 
-document.addEventListener('keydown', jump); // Corrija o tipo de evento para 'keydown'
